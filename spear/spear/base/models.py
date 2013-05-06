e134522d87de0aabc02e94194df9083e34024f59
Sergey Trifonov <svtrifonov@gmail.com>
2013-05-06 11:35:34 +0400

    added models: Application, Repository, Installation, Environment, Configuration, Task

--- spear/spear/base/models.py
+++ spear/spear/base/models.py
@@ -42,15 +42,84 @@ class SSHKey(models.Model):
     def __unicode__(self):
         return self.name
 
+class Application(models.Model):
+    name = models.TextField()
+    registered = models.DateTimeField()
+    owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
+class Repository(models.Model):
+    SVN = 0
+    GIT = 1
+    FTP = 2
+    TYPE_CHOICES = (
+        (0, 'Subversion'),
+        (1, 'Git'),
+        (2, 'FTP'),
+    )
+    name = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=SVN)
+    version = models.TextField()
+    meta = models.TextField()
+    registered = models.DateTimeField()
+    owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+    
+class Installation(models.Model):
+    node = models.ForeignKey(Node)
+    installed = models.DateTimeField()
+    app = models.ForeignKey(Application)
+    repo = models.ForeignKey(Repository)
+    
+class Environment(models.Model):
+    LOCAL = 0
+    PBS = 1
+    TYPE_CHOICES = (
+        (0, 'Local'),
+        (1, 'PBS'),
+    )
+    name = models.TextField()
+    owner = models.ForeignKey(User)
+    created = models.DateTimeField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=LOCAL)
+    meta = models.TextField()
+    
+    
+class Configuration(models.Model):
+    name = models.TextField()
+    owner = models.ForeignKey(User)
+    created = models.DateTimeField()
+    app = models.ForeignKey(Application)
+    app_version = models.TextField()
+    app_meta = models.TextField()
+    env_type = models.TextField()
+    env_meta = models.TextField()
+    in_files = models.TextField()
+    out_files = models.TextField()
+        
+    def __unicode__(self):
+        return self.name
+    
 class Task(models.Model):
-    nid = models.ForeignKey(Node, on_delete=models.CASCADE)
+    name = models.TextField()
+    launched = models.DateTimeField()
+    owner = models.ForeignKey(User)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
+    env = models.ForeignKey(Environment)
+    inst = models.ForeignKey(Installation)
+    conf = models.ForeignKey(Configuration)
+        
     status = models.IntegerField()
-    queue_type = models.TextField()
     exitstatus = models.IntegerField()
     workdir = models.TextField()
     executable = models.TextField()
     args = models.TextField()
-    queue_conf = models.TextField()
+    queue_type = models.TextField()
+    queue_meta = models.TextField()
     stdin = models.TextField()
     stdout = models.TextField()
     stderr = models.TextField()
