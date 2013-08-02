74fca8e1acb0cf0b20311981092642f13865ac02
Andrey Alekseenko <al42and@gmail.com>
2013-08-02 15:31:28 +0400

    Web interface: asocial authentication implemented
    Since using github for testing is somewhat too complicated, now one can
    create user with `./manage.py createsuperuser` and use it to login
    without need for various external services.

--- spear/spear/core/urls.py
+++ spear/spear/core/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.views.generic.simple import redirect_to
 from django.views.generic import TemplateView
 import django.contrib.auth.views
 
-urlpatterns = patterns('execmngr.views',
+urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name='core/home.html')),
 
     # Authentication
+    url(r'^login/$', 'spear.core.views.login', name='spear-core-login'),
     url(r'', include('social_auth.urls')),
-    url(r'^login/$', TemplateView.as_view(template_name='core/login.html'), name='spear-core-login'),
+    #url(r'^login/$', TemplateView.as_view(template_name='core/login.html'), name='spear-core-login'),
     url(r'^login-error/$', TemplateView.as_view(template_name='core/login-error.html')),
     url(r'^logout/$', django.contrib.auth.views.logout, name='spear-core-logout'),
-    #url(r'^profile/$', 'spear.core.views.profile'), # TODO: make first redirect to profile page 
+    #url(r'^profile/$', 'spear.core.views.profile'), # TODO: make first redirect to profile page
 )
--- spear/spear/core/views.py
+++ spear/spear/core/views.py
@@ -1,1 +1,35 @@
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django import forms
+from django.shortcuts import render
+from django.contrib import auth
+
+# Form to authenticate against django userbase
+class LoginForm(forms.Form):
+    userid = forms.CharField(label = 'User ID')
+    password = forms.CharField(label = 'Password', widget=forms.PasswordInput)
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        userid = cleaned_data.get('userid')
+        password = cleaned_data.get('password')
+        user = auth.authenticate(username=userid, password=password)
+        if user is not None:
+            if user.is_active:
+                cleaned_data['user'] = user
+                return cleaned_data
+            else:
+                self._errors['userid'] = self.error_class(['User blocked'])
+        else:
+            self._errors['password'] = self.error_class([u'Wrong ID or password'])
+        del cleaned_data['password']
+
+def login(request):
+    if request.method == 'POST':
+        # get userid, password
+        loginform = LoginForm(request.POST)
+        if loginform.is_valid():
+            auth.login(request, loginform.cleaned_data['user'])
+            return HttpResponseRedirect('/')
+    else:
+        loginform = LoginForm()
+    return render(request, 'core/login.html', {'authform':loginform})
+
